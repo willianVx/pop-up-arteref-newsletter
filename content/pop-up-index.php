ec6a3559587a99e37f054ea86151a2068c107c61
willianVx <willianvoliveiravx@gmail.com>
2019-02-05 13:10:02 -0200

    alterando texto

--- content/pop-up-index.php
+++ content/pop-up-index.php
@@ -5,13 +5,13 @@ function pop_up_index(){
             <div class="modal-dialog" role="document">
                 <div class="modal-content modal-arteref-pop-up">
                 <div class="modal-header">
-                    <h1 class="modal-title">Quer saber como as pessoas compraram arte em 2017-2018?</h1>
+                    <h1 class="modal-title">Como fazer marketing da sua galeria de arte</h1>
                     <button type="button" id="close_pop_up_arteref" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>O Arteref fez um resumo do relatório mundial de negócios online de arte, você quer receber? </p>
+                    <p>Baixe aqui o novo e-book do arteref</p>
                     <!-- <a href="http://arteref.com/downloads/relatorio%20de%20tedencias.pdf" download>Baixe aqui!</a> -->
                     <?php echo do_shortcode("[sibwp_form id=2]");?>
                 </div>
