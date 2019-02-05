b92128e6e531da0233b015481a472ac351c5a984
willianVx <willianvoliveiravx@gmail.com>
2019-02-05 16:07:08 -0200

    Ajustando layout

--- content/pop-up-index.php
+++ content/pop-up-index.php
@@ -6,16 +6,17 @@ function pop_up_index(){
                 <div class="modal-content modal-arteref-pop-up">
 
                     <div class="modal-header">
-                        <h1 class="modal-title">Como fazer marketing da sua galeria de arte</h1>
                         <button type="button" id="close_pop_up_arteref" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
 
                     <div class="modal-body">
-                        <p>Baixe aqui o novo e-book do arteref</p>
                         <div class="arteref_modal_content">
-                            <div>
+
+                            <div class="arteref_modal_content_form">
+                                <h3 class="modal-title">Como fazer marketing da sua galeria de arte</h3>
+                                <p>Baixe aqui o novo e-book do arteref</p>
                                 <!-- <a href="http://arteref.com/downloads/relatorio%20de%20tedencias.pdf" download>Baixe aqui!</a> -->
                                 <?php echo do_shortcode("[sibwp_form id=2]");?>
                             </div>
@@ -25,6 +26,7 @@ function pop_up_index(){
                                     <img src="https://arteref.com/wp-content/uploads/2019/02/banner-e-book.jpg" alt="Imagem e-book Arteref">
                                 </div>
                             </div>
+
                         </div>
                     </div>
 
